9d0e9f01a833d6e5e66b9dcffef0db7920d95152
lyx <doo8opnewman@outlook.com>
2024-06-18 17:26:50 +0800

    添加项目文件。

---
+++ T1/T1.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	char c;
+	int a;
+	while (cin >> c)
+	{
+		if (c == '@')break;
+		cin >> a;
+		for (int i = 0; i < a; i++)
+		{
+			if (i == a - 1)
+			{
+				for (int j = 0; j < 2*a-1; j++)
+				{
+					cout << c;
+				}
+				cout << endl;
+			}
+			else if (i == 0)
+			{
+				for (int k = 0; k < a-1; k++)
+				{
+					cout << " " ;
+				}
+				cout << c << endl;
+				
+			}
+			else
+			{
+				for (int k = 0; k < a-i-1 ; k++)
+				{
+					cout << " ";
+				}
+				cout << c;
+				for (int j = 0; j < 2 * i-1; j++)
+			    {
+				   cout << " ";
+			    }
+				cout << c << endl;
+			}
+			
+		}
+	}
+}
---
+++ school24.6.18/school24.6.18.cpp
@@ -0,0 +1,16 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	string s;
+	double sum=0;
+	while (cin>>s)
+	{
+		if (s[0]== '\n')break;
+		double a, b;
+		cin >> a >> b;
+		sum += a * b;
+	}
+	cout << fixed << setprecision(2) << ((int)(sum * pow(10, 2) + 5) / pow(10, 2));
+}
